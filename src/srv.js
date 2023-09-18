8b273828c9eee4574a11174223e84223474d2ae2
BasToTheMax <info@bastothemax.nl>
2023-09-18 20:27:06 +0200

    Start with plugin

--- src/srv.js
+++ src/srv.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 async function main(srv) {
     var [server, log, args] = srv;
@@ -7,6 +8,16 @@ async function main(srv) {
 
     log.info('Loading plugins...');
     log.debug(pluginPath);
+
+    if (!fs.existsSync(pluginPath)) fs.mkdirSync(pluginPath);
+
+    const getDirectories = source =>
+    fs.readdirSync(source, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
+
+    var plugins = getDirectories(pluginPath);
+    console.log(plugins);
 }
 
 module.exports = {
