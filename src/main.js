97616a3904cc705c7145c8ff447911b669ab3c0a
BasToTheMax <info@bastothemax.nl>
2023-09-20 20:19:13 +0200

    Comments

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
+// Import packages
 const mc = require('minecraft-protocol');
 const arg = require('arg');
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+// Logging
 const printer = require("fancy-printer");
 const log = printer.create({
 	styleSubstitutionsEnabled: true
 });
 
+// Logginf format
 log.setFormat(`%tag %text`);
+
+// Console arguments
 const args = arg({
 	// Types
 	'--help': Boolean,	
@@ -18,29 +23,39 @@ const args = arg({
 	'--offline': Boolean
 });
 
+// Main function
 async function main() {
+	// Clear the screen
 	console.clear();
-	console.log('Easter egg!');
-	await sleep(100);
-	console.clear();
+
+	// Help command
 	if (args['--help']) {
 		return console.log(require('./menus').help());
 	}
+	// Version command
 	if (args['--version']) {
 		return console.log(require('./menus').version());
 	}
 
+	// Get host and port from arguments
 	var port = args['--port'] ?? 25565;
 	var host = args['--host'] ?? '0.0.0.0';
+
+	// Offline mode
 	var online = args['--offline'] ?? true;
 	online = !online
 
+	// Log in console if offline mode is enabled
 	if (online == false) {
 		log.warn("Running insecure mode!")
 	}
 
+	// Use pterodactyl port
 	if (process.env['SERVER_PORT'] && process.env['P_SERVER_UUID']) {
+		// Logging
 		log.addTag("pterodactyl", "WINGS", "", "#959bdb", "#c1f7e2");
+
+		// Use ptero port
 		if (!args['--port']) {
 			log.tag("pterodactyl", "Pterodactyl detected! Using pterodactyl port.");
 			log.tag("pterodactyl", "This can be overwritten by --port");
@@ -52,6 +67,7 @@ async function main() {
 
 	log.info(`&eStarting minecraft server on &c${host}&e:&c${port}`);
 	
+	// Create mc server using protocol
 	const server = mc.createServer({
 		'online-mode': online,
 		encryption: true,
@@ -61,8 +77,10 @@ async function main() {
 		maxPlayers: 100
 	});
 
+	// Add log for plugins
 	log.addTag("plugin", "PLUGIN", "", "#424ef5", "");
 
+	// Start main app
 	require('./srv').main([
 		server,
 		log,
