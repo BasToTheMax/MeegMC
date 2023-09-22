1d858e51d9079c075f84a2effae04d72bddd37ec
BasToTheMax <info@bastothemax.nl>
2023-09-23 01:11:57 +0200

    Simple player class

--- src/context/Player.js
+++ src/context/Player.js
@@ -1,8 +1,22 @@
+const { Vec3 } = require("vec3");
+
 class Player {
     constructor(server, uuid, username) {
         this.server = server;
         this.uuid = uuid;
         this.username = username;
+
+        this.position = new Vec3(0, 0, 0);
+        this.look = { yaw: 0, pitch: 0 };
+        this.gamemode = 0;
+        this.health = 20;
+        this.food = 20;
+        this.xp = 0;
+        
+        this.heldItemSlot = 0;
+        this.onGround = true;
+
+        this.inventory = {};
     }
 
     setUsername(name) {
